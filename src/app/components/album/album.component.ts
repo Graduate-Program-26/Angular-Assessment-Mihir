dd88598aebf8140fef3471bf73ebe17f5076ef8f
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-05-04 09:57:04 +0200

    all the set up for the album component before designing it

--- src/app/components/album/album.component.ts
+++ src/app/components/album/album.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
+import { AlbumStore } from "../../store/album.store";
+import { ActivatedRoute } from "@angular/router";
+import { DeezerAlbumTrack } from "../../models/album.model";
 
 @Component({
     selector: 'app-album',
     standalone: true,
-    template: '',
+    templateUrl: 'album.component.html',
 })
-export class AlbumComponent {
+export class AlbumComponent implements OnInit {
+    protected readonly store = inject(AlbumStore);
+    protected readonly route = inject(ActivatedRoute);
 
+    ngOnInit(): void {
+        const id = Number(this.route.snapshot.paramMap.get('id'));
+        if (id) {
+            this.store.loadAlbum(id);
+        }
+    }
+
+    onPreview(track: DeezerAlbumTrack): void {
+        const audio = new Audio(track.preview);
+        void audio.play();
+    }
 }
