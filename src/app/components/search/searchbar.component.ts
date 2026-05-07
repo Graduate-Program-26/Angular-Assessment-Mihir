b0dcf59f591e67b7669e9eea9f361725954a7b1a
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-05-07 19:52:46 +0200

    fix: updated search page styling

--- src/app/components/search/searchbar.component.ts
+++ src/app/components/search/searchbar.component.ts
@@ -21,7 +21,7 @@ import { SearchStore } from '../../store/search.store';
       [value]="store.query()"
       (input)="onInput($event)"
       (focus)="focused.emit()"
-      class="w-full h-11 pl-11 pr-10 rounded-full text-muted-foreground bg-gray-200 text-sm placeholder:text-gray-400 border border-transparent backdrop-blur-md transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-white/20 focus:border-white/20 focus:bg-neutral-300 hover:bg-neutral-400
+      class="w-full h-11 pl-11 pr-10 bg-gray-200 rounded-full text-sm transition-all duration-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring
       "
     />
   </div>
