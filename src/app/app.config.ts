5e77e010cf77040d85946249b69e27ee532b7ea1
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-05-08 12:20:23 +0200

    fix: rollback to previous implementation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,10 @@ import { provideRouter } from '@angular/router';
 import { provideAuth0 } from '@auth0/auth0-angular';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withJsonpSupport } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient(), provideAuth0({
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient(withJsonpSupport()), provideAuth0({
     domain: "dev-0aachqmr51vhi734.us.auth0.com",
     clientId: "M7eVSjBhOzF7eqY9y2lsGrUjI6v7ISOP",
     authorizationParams: {
